7d9a72f14c0fd6eff363b6f78c508261411ec306
JeremyBois <JeremyBoisGit@pm.me>
2023-01-29 22:27:04 +0100

    ♻️ 👽️ - Move to data driven configuration (using `info.json`) instead of the combination of both `rules.mk` and `config.h`. Replace old keycode to new ones (reset, eepreset, debug).

--- config.h
+++ config.h
@@ -2,61 +2,16 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameters */
-#define VENDOR_ID 0x7466  // JB
-#define PRODUCT_ID 0x5053 // PS
-#define DEVICE_VER 0x0005
-#define MANUFACTURER JeremyBois
-#define PRODUCT Ptechinos
-
 /* Key matrix size */
 #define MATRIX_ROWS 8 // Rows are doubled-up
 #define MATRIX_COLS 5
-
-/*
- * Keyboard Matrix Assignments
- * ROWS: AVR pins used for rows, top to bottom
- * COLS: AVR pins used for columns, left to right
- * DIODE_DIRECTION :
- *  COL2ROW = COL = Anode(+), ROW = Cathode(-, marked on diode)
- *  ROW2COL = ROW = Anode(+), COL = Cathode(-, marked on diode)
- */
-#define MATRIX_ROW_PINS \
-    { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS \
-    { F7, B1, B3, B2, B6 }
-#define DIODE_DIRECTION COL2ROW
 
 /* Encoder support
  * Same pins / resolution on both sides
  * Encoders reference : EVQWGD001
  * https://github.com/qmk/qmk_firmware/blob/master/docs/feature_encoders.md
  */
-#define ENCODERS_PAD_A \
-    { F5 }
-#define ENCODERS_PAD_B \
-    { F4 }
-#define ENCODERS_PAD_A_RIGHT \
-    { F5 }
-#define ENCODERS_PAD_B_RIGHT \
-    { F4 }
-#define ENCODER_RESOLUTIONS \
-    { 4 }
-#define ENCODER_RESOLUTIONS_RIGHT \
-    { 4 }
 #define ENCODER_DEFAULT_POS 0x3
-
-/*
- * Split Keyboard specific options,
- * 'SPLIT_KEYBOARD = yes' should be your rules.mk
- */
-#define USE_SERIAL
-#define SOFT_SERIAL_PIN D2 // pin used for data communication between halves
-
-/* General */
-#define TAPPING_TERM 100
-#define DEBOUNCE 3  // Default is 5 (lower is better)
-#define TAP_CODE_DELAY 10
 
 /* VIA */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
--- rules.mk
+++ rules.mk
@@ -1,32 +1,4 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = caterina
-
-SPLIT_KEYBOARD = yes			# Split keyboard
-LTO_ENABLE = yes				# Save space by link time optimization
-
-# Options
-RGBLIGHT_ENABLE = no			# RGB lighting (+3826)
-ENCODER_ENABLE = yes		    # Rotary encoders (+1112)
-EXTRAKEY_ENABLE = yes			# Audio control and System control (+450)
-MOUSEKEY_ENABLE = yes			# Mouse keys (+1482). Not required for trackball usage
-COMBO_ENABLE = yes              # Allow to write combo
-
-# Unused
-BOOTMAGIC_ENABLE = no           # Software way to enter boot mode
-OLED_ENABLE = no                # Must be disabled if OLEDs are not installed (+3608-4774 with pet)
-CONSOLE_ENABLE = no             # Console for debug (+4686)
-UNICODE_ENABLE = no             # Allow to send unicode characters
-SPACE_CADET_ENABLE = no 		# Save space by not using space cadet left/right shift (+350)
-NKRO_ENABLE = no				# 6KRO by default. NKRO works if enabled (+328)
-TAP_DANCE_ENABLE = no           # Allow tap dance combinations
-LEADER_ENABLE = no              # Combinaisons with a leader key
-GRAVE_ESC_ENABLE = no
-
 # Saves a bunch of memory
-# EXTRAFLAGS += -flto
 VERBOSE = no
 DEBUG_MATRIX_SCAN_RATE  = no
 DEBUG_MATRIX = no
