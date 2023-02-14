829c2969b8db42c5a5a82dcdf25eee6fa95d6fcb
JeremyBois <JeremyBoisGit@pm.me>
2023-02-14 21:27:31 +0100

    💄 - More uniform comments layout.

--- 2040/rules.mk
+++ 2040/rules.mk
@@ -4,6 +4,9 @@ DEBUG_MATRIX_SCAN_RATE  = no
 DEBUG_MATRIX = no
 MAGIC_ENABLE = no
 
-# PIO serial drivers for RP2040
+# ┌─────────────────────────────────────────────────┐
+# │ PIO serial drivers for RP2040                   │
+# └─────────────────────────────────────────────────┘
 # https://github.com/qmk/qmk_firmware/blob/master/docs/serial_driver.md#usart-half-duplex
 SERIAL_DRIVER = vendor
+
