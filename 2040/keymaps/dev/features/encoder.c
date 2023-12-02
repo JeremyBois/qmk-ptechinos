13c147b0b1ef9101395f4ac3568eed23a67b1fdd
JeremyBois <JeremyBoisGit@pm.me>
2023-12-02 19:04:52 +0100

    ♻️ [2040] -  Use an hardcore delay on encoder keys. Required for function keys to be recognized by the host especially on Windows.

--- 2040/keymaps/dev/features/encoder.c
+++ 2040/keymaps/dev/features/encoder.c
@@ -3,34 +3,36 @@
 #    include "encoder.h"
 #    include "customCodes.h"
 
+// @NOTE Delay (>10) required on media keys to avoid computer to ignore / not receiving it
+
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Upper left encoder
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code_delay(KC_VOLU, 20);
         } else {
-            tap_code(KC_VOLD);
+            tap_code_delay(KC_VOLD, 20);
         }
     } else if (index == 1) {
         // Lower left encoder
         if (clockwise) {
-            tap_code16(C(A(KC_RIGHT)));
+            tap_code16_delay(C(A(KC_RIGHT)), 20);
         } else {
-            tap_code16(C(A(KC_LEFT)));
+            tap_code16_delay(C(A(KC_LEFT)), 20);
         }
     } else if (index == 2) {
         // Upper right encoder
         if (clockwise) {
-            tap_code16(C(A(KC_RIGHT)));
+            tap_code16_delay(C(A(KC_RIGHT)), 20);
         } else {
-            tap_code16(C(A(KC_LEFT)));
+            tap_code16_delay(C(A(KC_LEFT)), 20);
         }
     } else if (index == 3) {
         // Lower right encoder
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code_delay(KC_VOLU, 20);
         } else {
-            tap_code(KC_VOLD);
+            tap_code_delay(KC_VOLD, 20);
         }
     }
 
