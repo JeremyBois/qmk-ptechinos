e78197b910d48a8bd347b235f4435d5524426e1a
JeremyBois <JeremyBoisGit@pm.me>
2025-04-19 11:24:20 +0200

    ✨ [Switcher] - Add helper function to handle dead keys sequences and how dead key must be handled relative to active mods. Also make easier to detect if a mod is active.

--- 2040/keymaps/dev/features/utility.c
+++ 2040/keymaps/dev/features/utility.c
@@ -1,5 +1,6 @@
 #include "action.h"
 #include "action_util.h"
+#include "modifiers.h"
 #include "quantum.h"
 
 #include "utility.h"
@@ -7,7 +8,6 @@
 void tap_key_with_mods(uint16_t keycode, uint8_t mods) {
     uint8_t prev_mods = get_mods();
 
-    clear_mods();
     clear_keyboard();
     add_key(keycode);
     if (mods) set_mods(mods);
@@ -22,4 +22,38 @@ void tap_key_with_mods(uint16_t keycode, uint8_t mods) {
 void tap_undead_key(uint16_t code) {
     tap_code16(code);
     tap_code16(KC_SPACE);
+}
+
+void tap_with_dead_key_with_mods(uint16_t dead_code, uint16_t key_code, uint8_t mods, bool shift) {
+    tap_key_with_mods(dead_code, mods);
+    if (shift)
+        tap_code16(S(key_code));
+    else
+        tap_code16(key_code);
+}
+
+void tap_with_dead_key_no_mods(uint16_t dead_code, uint16_t key_code, bool shift) {
+    tap_key_with_mods(dead_code, 0U);
+    if (shift)
+        tap_code16(S(key_code));
+    else
+        tap_code16(key_code);
+}
+
+void tap_with_dead_key(uint16_t dead_code, uint16_t key_code, bool shift) {
+    tap_code16(dead_code);
+    if (shift)
+        tap_code16(S(key_code));
+    else
+        tap_code16(key_code);
+}
+
+
+bool is_weak_mod_active(uint8_t mod) {
+    return (get_weak_mods() & mod) == mod;
+}
+
+
+bool is_mod_active(uint8_t mods, uint8_t mod) {
+    return (mods & mod) == mod;
 }
--- 2040/keymaps/dev/features/utility.h
+++ 2040/keymaps/dev/features/utility.h
@@ -4,6 +4,7 @@
 
 /**
  * @brief      Force a specific set of modifier when taping a key.
+ *             Useful for example to get È when shift is enabled instead of Ẽ
  * @note       https://github.com/qmk/qmk_firmware/issues/1708#issuecomment-432442389
  *
  * @param[in]  keycode  The keycode
@@ -17,3 +18,19 @@ void tap_key_with_mods(uint16_t keycode, uint8_t mods);
  * @param[in]  code  The code
  */
 void tap_undead_key(uint16_t code);
+
+/**
+ * @brief      Combine a dead key with a normal one.
+ *
+ * @param[in]  dead_code  The dead key code
+ * @param[in]  key_code   The normal key code
+ * @param[in]  mods       Mods that must be added to the dead code
+ * @param[in]  shift      True to shift the key code
+ */
+void tap_with_dead_key_with_mods(uint16_t dead_code, uint16_t key_code, uint8_t mods, bool shift);
+void tap_with_dead_key_no_mods(uint16_t dead_code, uint16_t key_code, bool shift);
+void tap_with_dead_key(uint16_t dead_code, uint16_t key_code, bool shift);
+
+
+bool is_weak_mod_active(uint8_t mod);
+bool is_mod_active(uint8_t mods, uint8_t mod);
